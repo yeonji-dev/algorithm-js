f257c47e492b2ad25cf593b4d9a882194ab60f00
yeonji-dev <lilianaaa804@gmail.com>
2023-09-26 13:58:02 +0900

    Config: 문제&답 실행 환경

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-import {runTest1} from "./src/test.js";
+if (process.argv.length < 4){
+    console.error('실행할 문제를 인수로 입력해주세요.');
+    console.error('실행 커맨드 형식: node index.js 레벨 문제명');
+    console.error('레벨 및 문제명은 README.md 파일을 참조하세요.')
+    process.exit(1);
+}
 
-const input = "abcde";
+const packageName = process.argv[2];
+const js = process.argv[3];
 
-const answer = runTest1(input);
+const {question, input, run} = await import(`./src/${packageName}/${js}.js`);
 
+const answer = run(input);
+
+console.log("===============QUESTION===============")
+console.log(question);
 console.log("================ANSWER================");
 console.log(answer);
 console.log("======================================");
--- src/lv1/test.js
+++ src/lv1/test.js
@@ -1,4 +1,6 @@
-export const runTest1 = (s) => {
+export const question = `가운데 문자열 뽑기`;
+export const input = "abcde";
+export const run = (s) => {
     const length = s.length;
     const n = parseInt(length / 2);
     return length % 2 === 0
